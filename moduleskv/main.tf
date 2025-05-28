a547e7bfb7f4607ff640df01869a76afc576706b
Diego Vallejos <dvallejosgalvez@gmail.com>
2025-05-27 22:43:31 -0400

    update only to module

--- moduleskv/main.tf
+++ moduleskv/main.tf
@@ -9,133 +9,199 @@ resource "azurerm_key_vault" "kvdvorg" {
 
   sku_name = "standard"
 
-
-  access_policy {
-    tenant_id = var.tenant
-    object_id = var.azclioid
-
-    key_permissions = [
-        "Get",
-        "List",
-        "Update",
-        "Create",
-        "Import",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-        "GetRotationPolicy",
-        "SetRotationPolicy",
-        "Rotate",
-        "Encrypt",
-        "Decrypt",
-        "UnwrapKey",
-        "WrapKey",
-        "Verify",
-        "Sign",
-        "Purge",
-        "Release",
-    ]
-
-    secret_permissions = [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-        "Purge",
-    ]
-
-    certificate_permissions = [
-        "Get",
-        "List",
-        "Update",
-        "Create",
-        "Import",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-        "ManageContacts",
-        "ManageIssuers",
-        "GetIssuers",
-        "ListIssuers",
-        "SetIssuers",
-        "DeleteIssuers",
-        "Purge",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
-
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = "7ee7aecd-1ec9-4ace-afca-c2cd12c60500"
-
-    key_permissions = [
-        "Get",
-        "List",
-        "Update",
-        "Create",
-        "Import",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-        "GetRotationPolicy",
-        "SetRotationPolicy",
-        "Rotate",
-        "Encrypt",
-        "Decrypt",
-        "UnwrapKey",
-        "WrapKey",
-        "Verify",
-        "Sign",
-        "Purge",
-        "Release",
-    ]
-
-    secret_permissions = [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-        "Purge",
-    ]
-
-    certificate_permissions = [
-        "Get",
-        "List",
-        "Update",
-        "Create",
-        "Import",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-        "ManageContacts",
-        "ManageIssuers",
-        "GetIssuers",
-        "ListIssuers",
-        "SetIssuers",
-        "DeleteIssuers",
-        "Purge",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
-
 }
 
+resource "azurerm_key_vault_access_policy" "azcli_access_policy" {
+  key_vault_id = azurerm_key_vault.kvdvorg.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = var.azclioid
+
+  key_permissions = [
+      "Get",
+      "List",
+      "Update",
+      "Create",
+      "Import",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "GetRotationPolicy",
+      "SetRotationPolicy",
+      "Rotate",
+      "Encrypt",
+      "Decrypt",
+      "UnwrapKey",
+      "WrapKey",
+      "Verify",
+      "Sign",
+      "Purge",
+      "Release",
+  ]
+
+  secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
+  ]
+
+  certificate_permissions = [
+      "Get",
+      "List",
+      "Update",
+      "Create",
+      "Import",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "ManageContacts",
+      "ManageIssuers",
+      "GetIssuers",
+      "ListIssuers",
+      "SetIssuers",
+      "DeleteIssuers",
+      "Purge",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "user_access_policy" {
+  key_vault_id = azurerm_key_vault.kvdvorg.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = "7ee7aecd-1ec9-4ace-afca-c2cd12c60500"
+
+  key_permissions = [
+      "Get",
+      "List",
+      "Update",
+      "Create",
+      "Import",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "GetRotationPolicy",
+      "SetRotationPolicy",
+      "Rotate",
+      "Encrypt",
+      "Decrypt",
+      "UnwrapKey",
+      "WrapKey",
+      "Verify",
+      "Sign",
+      "Purge",
+      "Release",
+  ]
+
+  secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
+  ]
+
+  certificate_permissions = [
+      "Get",
+      "List",
+      "Update",
+      "Create",
+      "Import",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "ManageContacts",
+      "ManageIssuers",
+      "GetIssuers",
+      "ListIssuers",
+      "SetIssuers",
+      "DeleteIssuers",
+      "Purge",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "des_access_policy" {
+  count               = var.des_enabled ? 1 : 0
+  key_vault_id = azurerm_key_vault.kvdvorg.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = azurerm_disk_encryption_set.desdvorg[0].identity[0].principal_id
+
+  key_permissions = [
+      "Get",
+      "List",
+      "Update",
+      "Create",
+      "Import",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "GetRotationPolicy",
+      "SetRotationPolicy",
+      "Rotate",
+      "Encrypt",
+      "Decrypt",
+      "UnwrapKey",
+      "WrapKey",
+      "Verify",
+      "Sign",
+      "Purge",
+      "Release",
+  ]
+
+  secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
+  ]
+
+  certificate_permissions = [
+      "Get",
+      "List",
+      "Update",
+      "Create",
+      "Import",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "ManageContacts",
+      "ManageIssuers",
+      "GetIssuers",
+      "ListIssuers",
+      "SetIssuers",
+      "DeleteIssuers",
+      "Purge",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+}
 
 data "azurerm_client_config" "current" {}
